4e1da7d549c751fb2f2c6db200bb74a8eaac88ff
tje0714 <tje0714@outlook.kr>
2025-07-30 16:07:10 +0900

    Ajax를 활용한 로그인 로그아웃 실습

--- js/12_Ajax_login.js
+++ js/12_Ajax_login.js
@@ -18,8 +18,41 @@ $(function () {
       success: function (data) {
         console.log("성공적으로 json에서 가져온 데이터 확인하기 : ", data);
         // 만약에 json에서 확인한 데이터와 소비자가 작성한 데이터가 일치한다면
-        /*
+        if (data.users[abc]) {
+          //사용자가 존재할 때
 
+          if (data.users[abc].password === password) {
+            // 사용자도 존재하고, 비밀번호도 일치한다면
+            $("#result").html(
+              `로그인성공! 환영합니다. ${data.users[abc].name} 님`
+            );
+            $("#로그인기능").hide();
+            $("#로그아웃기능").show();
+          } else {
+            // 사용자가 존재하지만, 비밀번호가 일치하지 않을 때
+            $("#result").html("일치하는  비밀번호가 없습니다.");
+          }
+        } else {
+          //  사용자가 존재하지 않을 때
+          $("#result").html("존재하는 아이디가 없습니다.");
+        }
+      },
+      error: function () {
+        alert("데이터 가져오는데 실패했습니다.");
+      },
+    });
+  });
+
+  $("#로그아웃기능").click(function (e) {
+    e.preventDefault(); // 로그아웃 button 태그 내부에 type = submit 로 설정되어 있어, 제출방지
+    $("로그인기능").show();
+    $("로그아웃기능").hide();
+    $("#username").val("");
+    $("#password").val("");
+    $("#result").html("로그아웃이 완료되었습니다.");
+  });
+});
+/*
         javascript 에서 [] . 으로 데이터를 주고 받을 때의 차이
         users[abc] = abc의 변수의 값이 admin인 속성을 찾을 때 사용
 
@@ -32,25 +65,3 @@ $(function () {
         예를 들어 username.value 값으로 admin 이 들어왔을 경우
         users[abc] -> users[admin] 으로 변경되어 admin과 일치하는 아이디를 검색
         */
-        if (data.users[abc].password === password) {
-          $("#result").html(
-            `로그인성공! 환영합니다. ${data.users[abc].name} 님`
-          );
-          $("#로그인기능").hide();
-          $("#로그아웃기능").show();
-        } else {
-          $("#result").html("일치하는 아이디나 비밀번호가 없습니다.");
-        }
-      },
-      error: function () {
-        alert("데이터 가져오는데 실패했습니다.");
-      },
-    });
-  });
-
-  $("#로그아웃기능").click(function(e) {
-    // 로그아웃을 진행할 경우
-    // 로그인기능 show 보여주기
-    // 로그아웃기능 hide 숨기기 설정
-  })
-});
